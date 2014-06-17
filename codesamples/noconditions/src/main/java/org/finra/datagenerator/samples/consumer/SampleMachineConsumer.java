58da2c14194228f7a3660cea768d65083f245ef4
SMxJrz <Smelecs@gmail.com>
2014-06-17 16:40:00 -0400

    expand dg noconditions example to use map/reduce on the sample machines

--- codesamples/noconditions/src/main/java/org/finra/datagenerator/samples/consumer/SampleMachineConsumer.java
+++ codesamples/noconditions/src/main/java/org/finra/datagenerator/samples/consumer/SampleMachineConsumer.java
@@ -13,10 +13,15 @@
  */
 package org.finra.datagenerator.samples.consumer;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.Random;
+
+import org.apache.hadoop.io.NullWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper.Context;
 import org.apache.log4j.Logger;
 import org.finra.datagenerator.consumer.DataConsumer;
 import org.finra.datagenerator.consumer.defaults.ConsumerResult;
@@ -26,14 +31,22 @@ public class SampleMachineConsumer implements DataConsumer {
     protected static final Logger log = Logger.getLogger(SampleMachineConsumer.class);
 
     private final int myTemplateHashCode = "#{customplaceholder}".hashCode();
+    private final int divideBy2HashCode = "#{divideBy2}".hashCode();
     private final Random rand = new Random(System.currentTimeMillis());
-
+    private final Context context;
+    public SampleMachineConsumer(){
+    	this.context = null;
+    }
+    public SampleMachineConsumer(Context context){
+    	this.context = context;
+    }
+    
     @Override
     public void consume(ConsumerResult cr) {
-        // Go through our templates and fill them with values
+    	// Go through our templates and fill them with values
         Map<String, String> originalRow = cr.getDataMap();
         HashMap<String, String> outputValues = new HashMap<>();
-
+        
         for (Entry<String, String> entry : originalRow.entrySet()) {
             String value = entry.getValue();
             int hashCode = value.hashCode();
@@ -43,12 +56,29 @@ public class SampleMachineConsumer implements DataConsumer {
                 int ran = rand.nextInt();
                 value = "" + ran;
             }
-
+            
+            /*
+             * Enhanced Demo
+             */
+            if (hashCode == divideBy2HashCode && value.equals("#{divideBy2}")) {
+                value = "" + Integer.valueOf(outputValues.get("var_out_V3")) / 2;
+            }
             outputValues.put(entry.getKey(), value);
         }
 
         // Using the values, compile our output. In our case, we will just write it to the console
-        System.out.println("Row=" + outputValues.toString());
+        if(context == null){
+        	System.out.println("Row=" + outputValues.toString());
+        //for HDFS we each mapper will write to thier own context
+        }else{
+        	try {
+				context.write(NullWritable.get(), new Text(outputValues.toString()));
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+        }
     }
 
 }
---
+++ codesamples/noconditions/src/main/java/org/finra/datagenerator/samples/distributor/hdfs/DataGeneratorMapper.java
@@ -0,0 +1,74 @@
+package org.finra.datagenerator.samples.distributor.hdfs;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.log4j.Logger;
+import org.finra.datagenerator.distributor.SearchProblem;
+import org.finra.datagenerator.distributor.multithreaded.DefaultDistributor;
+import org.finra.datagenerator.samples.consumer.SampleMachineConsumer;
+
+import com.google.gson.Gson;
+
+/**
+ * Created by robbinbr on 4/7/2014.
+ */
+public class DataGeneratorMapper extends Mapper<LongWritable, Text, LongWritable, Text> {
+
+    protected static final Logger log = Logger.getLogger(DataGeneratorMapper.class);
+    private String reportingHost;
+
+    private final DefaultDistributor distributor = new DefaultDistributor();
+
+    @Override
+    public void setup(final Context context) {
+
+        //DataConsumer dataConsumer = gson.fromJson(context.getConfiguration().get("dataConsumer"), DataConsumer.class);
+        //distributor.setDataConsumer(dataConsumer);
+        distributor.setStateMachineText(context.getConfiguration().get("stateMachineText"));
+        distributor.setMaxNumberOfLines(context.getConfiguration().getLong("maxNumberOfLines", 100000));
+
+        reportingHost = context.getConfiguration().get("reportingHost", "NOTFOUND");
+        log.info("Reporting host is:" + reportingHost);
+    }
+
+
+    @Override
+    public void map(final LongWritable key, Text value, final Context context) throws IOException,
+            InterruptedException {
+
+    	log.info("Entering the mapper");
+        // Prepare DataConsumer
+        SampleMachineConsumer wrappingConsumer = new SampleMachineConsumer(context);
+        distributor.setDataConsumer(wrappingConsumer);
+        distributor.setExitFlag(new AtomicBoolean(false));
+
+        // Prepare Problems (only one)
+        List<SearchProblem> problemList = new ArrayList<SearchProblem>();
+        SearchProblem problem = SearchProblem.fromJson(value.toString());
+        log.info(value.toString());
+        problemList.add(problem);
+        log.info("We have " + problemList.size() + " Problems");
+        log.info("Distributing problems");
+        // Execute
+        distributor.distribute(problemList);
+
+        log.info("Writing results");
+
+        // Write results to context
+/*        for (String line : wrappingConsumer.getResults()) {
+         long val = counter.getAndIncrement();
+         log.info(val + "," + line);
+         context.write(new LongWritable(val), new Text(line));
+         }*/
+    }
+}
