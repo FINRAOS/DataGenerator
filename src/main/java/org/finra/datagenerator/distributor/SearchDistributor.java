7c04bc6faae1a0f2a30099527764aa06967a7e52
Mohamed Ibrahim <mibrahim@mibrahim.net>
2014-08-01 06:35:57 -0400

    CheckStyle fixes

--- src/main/java/org/finra/datagenerator/distributor/SearchDistributor.java
+++ src/main/java/org/finra/datagenerator/distributor/SearchDistributor.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014 DataGenerator Contributors.
+ * Copyright 2014 DataGenerator Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -24,11 +24,37 @@ import org.finra.datagenerator.consumer.DataConsumer;
  */
 public interface SearchDistributor {
 
+    /**
+     * Sets the data consumer
+     *
+     * @param dataConsumer the DataConsumer
+     * @return a reference to the current SearchDistributor
+     */
     SearchDistributor setDataConsumer(DataConsumer dataConsumer);
 
+    /**
+     * Sets the state machine XML
+     *
+     * @param stateMachineText a String containing the state machine XML
+     * @return a reference to the current SearchDistributor
+     */
     SearchDistributor setStateMachineText(String stateMachineText);
 
+    /**
+     * Distributes the list of the problems
+     *
+     * @param searchProblemList a list containing the search problems to
+     * distribute
+     */
     void distribute(List<SearchProblem> searchProblemList);
 
+    /**
+     * @todo Buggy !! this function is adding a new instance of AromicBoolen and
+     * not setting the value of a current atomic boolean
+     * @param name the name of the flag
+     * @param flag the value of the flag
+     *
+     * @TODO: Redesign
+     */
     void setFlag(String name, AtomicBoolean flag);
 }
