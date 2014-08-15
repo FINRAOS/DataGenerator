988d6f8fe4ba42010da052ad8f4c3716ae287bfe
yukaReal <yuriy.yankop@gmail.com>
2014-08-15 01:08:23 -0400

    rename archetypes package due to new package name rule

--- archetypes/useArhetype.sh
+++
@@ -1,6 +0,0 @@
-mvn archetype:generate                                  \
-  -DarchetypeGroupId=org.finra.datagenerator.samples    \
-  -DarchetypeArtifactId=noconditions-archetype          \
-  -DarchetypeVersion=1.1-SNAPSHOT                       \
-  -DgroupId=org.company                                 \
-  -DartifactId=DG_example_nocondition
