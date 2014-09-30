ce02204b3946339a4ff44a7310493db85a82c6c0
Mohamed Ibrahim <mibrahim@mibrahim.net>
2014-09-30 14:59:06 -0400

    Bump up the version to 2.0-SNAPSHOT
    Signed-off-by: Mohamed Ibrahim <mibrahim@mibrahim.net>

--- dg-archetypes/useArhetype.sh
+++ dg-archetypes/useArhetype.sh
@@ -1,6 +1,6 @@
 mvn archetype:generate                                  \
   -DarchetypeGroupId=org.finra.datagenerator.samples    \
   -DarchetypeArtifactId=noconditions-archetype          \
-  -DarchetypeVersion=1.1-SNAPSHOT                       \
+  -DarchetypeVersion=2.0-SNAPSHOT                       \
   -DgroupId=org.company                                 \
   -DartifactId=DG_example_nocondition
