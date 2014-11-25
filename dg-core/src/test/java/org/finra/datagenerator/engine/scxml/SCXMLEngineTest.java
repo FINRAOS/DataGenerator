eda8dbe3f65ddc87b4d1799867142f27a5cc27aa
yukaReal <yuriy.yankop@gmail.com>
2014-11-24 23:25:02 -0500

    added / improved tests for set inside dg:assign tag. Part of #159
    Signed-off-by: yukaReal <yuriy.yankop@gmail.com>

--- dg-core/src/test/java/org/finra/datagenerator/engine/scxml/SCXMLEngineTest.java
+++ dg-core/src/test/java/org/finra/datagenerator/engine/scxml/SCXMLEngineTest.java
@@ -21,16 +21,20 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.InputStream;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
- * Marshall Peters
- * Date: 9/3/14
+ * Marshall Peters Date: 9/3/14
  */
 public class SCXMLEngineTest {
+    private String[] setRecordType1 = new String[]{"a1", "b1", "c1", "d1", "e1", "f1", "g1"};
+    private String[] setRecordType2 = new String[]{"a2", "b2", "c2", "d2", "e2", "f2", "g2"};
+    private String[] setRecordType3 = new String[]{"a3", "b3", "c3", "d3", "e3", "f3", "g3"};
 
     /**
-     * Multiple variable assignments using set:{}
+     * Multiple variable assignments using 'set' inside 'dg:assign' tag
      */
     @Test
     public void testMultiVariableAssignment() {
@@ -40,17 +44,52 @@ public class SCXMLEngineTest {
 
         try {
             List<PossibleState> bfs = e.bfs(100);
-            Assert.assertEquals(343, bfs.size());  //7^3, produced by expanding BULK_ASSIGN
+            Assert.assertEquals("Oops! We have '" + bfs.size() + "' records, but we wait for 7^3, "
+                    + "produced by expanding BULK_ASSIGN", 343, bfs.size());
 
-            for (PossibleState p: bfs) {
+            Set<Set<String>> allPossibleValuesForRecordType123 = generateAllPossibleValues();
+            for (PossibleState p : bfs) {
+                // let's remove this generated record from list of all necessary records for this test
+                Set<String> recordType123 = new HashSet<String>();
+                recordType123.add(p.variables.get("var_out_RECORD_TYPE"));
+                recordType123.add(p.variables.get("var_out_RECORD_TYPE_2"));
+                recordType123.add(p.variables.get("var_out_RECORD_TYPE_3"));
+                if (allPossibleValuesForRecordType123.contains(recordType123)) {
+                    allPossibleValuesForRecordType123.remove(recordType123);
+                } else {
+                    Assert.fail("Oops! We wait for '" + recordType123 + "' combination for RECORD_TYPE_1, _2 and _3, but it wasn't generated!");
+                }
+
                 Assert.assertEquals(p.nextState.getId(), "ASSIGN_WITH_CONDITIONS");
                 Assert.assertEquals(p.variables.get("var_out_RECORD_TYPE_4"), "Lorem");
                 Assert.assertEquals(p.variables.get("var_out_RECORD_TYPE_5"), "Ipsum");
                 Assert.assertEquals(p.variables.get("var_out_RECORD_TYPE_6"), "Doler");
             }
+
+            // we have removed all generated values for RECORD_TYPE_1, _2 and _3, so 'allPossibleValuesForRecordType123' has be empty now
+            for (Set<String> valuesForRecordType123 : allPossibleValuesForRecordType123) {
+                Assert.fail("Oops! We have not generated combanation for for RECORD_TYPE_1, _2 and _3 - '" + valuesForRecordType123 + "'!");
+            }
         } catch (ModelException ex) {
-            Assert.fail();
+            Assert.fail("Oops! ModelException = " + e);
         }
+    }
+
+    private Set<Set<String>> generateAllPossibleValues() {
+        Set<Set<String>> result = new HashSet<Set<String>>();
+
+        for (String val1 : setRecordType1) {
+            for (String val2 : setRecordType2) {
+                for (String val3 : setRecordType3) {
+                    Set<String> record = new HashSet<String>();
+                    record.add(val1);
+                    record.add(val2);
+                    record.add(val3);
+                    result.add(record);
+                }
+            }
+        }
+        return result;
     }
 
     /**
@@ -79,7 +118,7 @@ public class SCXMLEngineTest {
             Assert.assertEquals(p.variables.get("var_out_RECORD_TYPE_8"), "");
             Assert.assertEquals(p.variables.keySet().size(), 8);
         } catch (ModelException ex) {
-            Assert.fail();
+            Assert.fail("Oops! ModelException = " + e);
         }
     }
 
@@ -92,11 +131,14 @@ public class SCXMLEngineTest {
         InputStream is = SCXMLEngineTest.class.getResourceAsStream("/bigtest.xml");
         e.setModelByInputFileStream(is);
 
+        String errorMessageExpected = "Could not achieve required bootstrap without reaching end state";
         try {
-            List<PossibleState> bfs = e.bfs(4000); //too many
-            Assert.fail();
+            e.bfs(4000); // too many
+            Assert.fail("Oops! We wait for '" + errorMessageExpected + "' error message here "
+                    + "(bootstrap size is too big for this model), but don't have it!");
         } catch (ModelException ex) {
-            Assert.assertEquals(ex.getMessage(), "Could not achieve required bootstrap without reaching end state");
+            Assert.assertEquals("Oops! We have '' modelException error message, but we wait for "
+                    + "'" + errorMessageExpected + "' here!", ex.getMessage(), errorMessageExpected);
         }
     }
 
