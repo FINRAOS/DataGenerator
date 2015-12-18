cc4cd9f7d209cfdc9439ce6f886183d813352e74
brijeshrpatel9 <brijeshrpatel9@gmail.com>
2015-12-19 02:18:42 +0530

    Fixed SCXMLFrontier

--- dg-core/src/main/java/org/finra/datagenerator/engine/scxml/tags/FileExtension.java
+++ dg-core/src/main/java/org/finra/datagenerator/engine/scxml/tags/FileExtension.java
@@ -16,7 +16,11 @@
 
 package org.finra.datagenerator.engine.scxml.tags;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
