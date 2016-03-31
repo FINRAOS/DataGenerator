61640e32ae7475e3a736b4629fc4b2096c893a48
Nathaniel Lee <nathaniel.lee@finra.org>
2016-03-31 19:14:15 -0400

    Date stuff

--- dg-core/src/main/java/org/finra/datagenerator/engine/scxml/tags/boundary/BoundaryDate.java
+++ dg-core/src/main/java/org/finra/datagenerator/engine/scxml/tags/boundary/BoundaryDate.java
@@ -15,11 +15,18 @@
  */
 package org.finra.datagenerator.engine.scxml.tags.boundary;
 
+import org.finra.datagenerator.consumer.EquivalenceClassTransformer;
 import org.finra.datagenerator.engine.scxml.tags.CustomTagExtension;
 import org.finra.datagenerator.engine.scxml.tags.boundary.action.BoundaryActionDate;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormatter;
+import org.joda.time.format.ISODateTimeFormat;
 
+import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
@@ -58,24 +65,6 @@ public abstract class BoundaryDate<T extends BoundaryActionDate> implements Cust
     }
 
     /**
-     *
-     * @param year to check if it is a leap year
-     * @return true if the year is a leap year
-     */
-    public boolean isLeapYear(int year) {
-        if (year % 4 == 0) {
-            if (year % 100 == 0) {
-                if (year % 400 == 0) {
-                    return true;
-                }
-            } else {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    /**
      * @param isNullable isNullable
      * @param earliest   lower boundary date
      * @param latest     upper boundary date
@@ -84,96 +73,19 @@ public abstract class BoundaryDate<T extends BoundaryActionDate> implements Cust
     public List<String> positiveCase(boolean isNullable, String earliest, String latest) {
         List<String> values = new LinkedList<>();
 
-        int earlyDay = Integer.parseInt(earliest.substring(8, 10));
-        int earlyMonth = Integer.parseInt(earliest.substring(5, 7));
-        int earlyYear = Integer.parseInt(earliest.substring(0, 4));
-        boolean isLeapYear = isLeapYear(earlyYear);
+        DateTimeFormatter parser = ISODateTimeFormat.date();
+        DateTime earlyDate = parser.parseDateTime(earliest);
+        DateTime lateDate = parser.parseDateTime(latest);
 
-        // prepend 0 for months 1 - 9
-        String earlyMo = (Integer.toString(earlyMonth).length() < 2 ? "0" + earlyMonth : "" + earlyMonth);
-        String earlyDy = (Integer.toString(earlyDay).length() < 2 ? "0" + earlyDay : "" + earlyDay);
+        String earlyDay = parser.print(earlyDate);
+        String nextDay = parser.print(earlyDate.plusDays(1));
+        String prevDay = parser.print(lateDate.minusDays(1));
+        String lateDay = parser.print(lateDate);
 
-        values.add("" + earlyYear + "-" + earlyMo + "-" + earlyDy);
-
-        if ((earlyMonth == 1 || earlyMonth == 3 || earlyMonth == 5 || earlyMonth == 7
-            || earlyMonth == 8 || earlyMonth == 10 || earlyMonth == 12) && earlyDay < 31) {
-            earlyDay++;
-        } else if ((earlyMonth == 4 || earlyMonth == 6 || earlyMonth == 9 || earlyMonth == 11)
-            && earlyDay < 30) {
-            earlyDay++;
-        } else if (earlyMonth == 2) {
-            if (isLeapYear && earlyDay < 29) {
-                earlyDay++;
-            } else if (!isLeapYear && earlyDay < 28) {
-                if (earlyDay < 28) {
-                    earlyDay++;
-                } else {
-                    earlyDay = 1;
-                    earlyMonth = 3;
-                }
-            } else {
-                earlyDay = 1;
-                earlyMonth = 3;
-            }
-        } else {
-            if (earlyMonth < 12) {
-                earlyDay = 1;
-                earlyMonth++;
-            } else {
-                earlyMonth = 1;
-                earlyDay = 1;
-                earlyYear++;
-            }
-        }
-
-        earlyMo = (Integer.toString(earlyMonth).length() < 2 ? "0" + earlyMonth : "" + earlyMonth);
-        earlyDy = (Integer.toString(earlyDay).length() < 2 ? "0" + earlyDay : "" + earlyDay);
-
-        values.add("" + earlyYear + "-" + earlyMo + "-" + earlyDy);
-
-        int lateDay = Integer.parseInt(latest.substring(8, 10));
-        int lateMonth = Integer.parseInt(latest.substring(5, 7));
-        int lateYear = Integer.parseInt(latest.substring(0, 4));
-        isLeapYear = isLeapYear(lateYear);
-
-        if (lateDay > 1) {
-            lateDay--;
-        } else {
-            if (lateMonth == 2 || lateMonth == 4 || lateMonth == 6 || lateMonth == 8
-                || lateMonth == 9 || lateMonth == 11) {
-                lateDay = 31;
-            } else if (lateMonth == 5 || lateMonth == 7 || lateMonth == 10 || lateMonth == 12) {
-                lateDay = 30;
-            } else if (lateMonth == 3) {
-                if (isLeapYear) {
-                    lateDay = 29;
-                    lateMonth = 2;
-                } else {
-                    lateDay = 28;
-                    lateMonth = 2;
-                }
-            } else {
-                if (lateYear > 1970) {
-                    lateMonth = 12;
-                    lateDay = 31;
-                    lateYear--;
-                }
-            }
-        }
-
-        String lateMo = (Integer.toString(lateMonth).length() < 2 ? "0" + lateMonth : "" + lateMonth);
-        String lateDy = (Integer.toString(lateDay).length() < 2 ? "0" + lateDay : "" + lateDay);
-
-        values.add("" + lateYear + "-" + lateMo + "-" + lateDy);
-
-        lateDay = Integer.parseInt(latest.substring(8, 10));
-        lateMonth = Integer.parseInt(latest.substring(5, 7));
-        lateYear = Integer.parseInt(latest.substring(0, 4));
-
-        lateMo = (Integer.toString(lateMonth).length() < 2 ? "0" + lateMonth : "" + lateMonth);
-        lateDy = (Integer.toString(lateDay).length() < 2 ? "0" + lateDay : "" + lateDay);
-
-        values.add("" + lateYear + "-" + lateMo + "-" + lateDy);
+        values.add(earlyDay);
+        values.add(nextDay);
+        values.add(prevDay);
+        values.add(lateDay);
 
         if (isNullable) {
             values.add("");
@@ -190,86 +102,62 @@ public abstract class BoundaryDate<T extends BoundaryActionDate> implements Cust
      */
     public List<String> negativeCase(boolean isNullable, String earliest, String latest) {
         List<String> values = new LinkedList<>();
-        int earlyDay = Integer.parseInt(earliest.substring(8, 10));
-        int earlyMonth = Integer.parseInt(earliest.substring(5, 7));
-        int earlyYear = Integer.parseInt(earliest.substring(0, 4));
-        boolean isLeapYear = isLeapYear(earlyYear);
 
-        if (earlyDay > 1) {
-            earlyDay--;
-        } else {
-            if (earlyMonth == 2 || earlyMonth == 4 || earlyMonth == 6 || earlyMonth == 8 || earlyMonth == 9
-                || earlyMonth == 11) {
-                earlyDay = 31;
-            } else if (earlyMonth == 5 || earlyMonth == 7 || earlyMonth == 10) {
-                earlyDay = 30;
-            } else if (earlyMonth == 3) {
-                if (isLeapYear) {
-                    earlyDay = 29;
-                    earlyMonth--;
-                } else {
-                    earlyDay = 28;
-                    earlyMonth--;
-                }
-            } else {
-                earlyMonth = 12;
-                earlyDay = 31;
-                earlyYear--;
-            }
-        }
+        DateTimeFormatter parser = ISODateTimeFormat.date();
+        DateTime earlyDate = parser.parseDateTime(earliest);
+        DateTime lateDate = parser.parseDateTime(latest);
 
-        String earlyMo = (Integer.toString(earlyMonth).length() < 2 ? "0" + earlyMonth : "" + earlyMonth);
-        String earlyDy = (Integer.toString(earlyDay).length() < 2 ? "0" + earlyDay : "" + earlyDay);
-        values.add("" + earlyYear + "-" + earlyMo + "-" + earlyDy);
+        String prevDay = parser.print(earlyDate.minusDays(1));
+        String nextDay = parser.print(lateDate.plusDays(1));
 
-        int lateDay = Integer.parseInt(latest.substring(8, 10));
-        int lateMonth = Integer.parseInt(latest.substring(5, 7));
-        int lateYear = Integer.parseInt(latest.substring(0, 4));
-
-        if ((lateMonth == 1 || lateMonth == 3 || lateMonth == 5 || lateMonth == 7
-            || lateMonth == 8 || lateMonth == 10 || lateMonth == 12) && lateDay < 31) {
-            lateDay++;
-        } else if ((lateMonth == 4 || lateMonth == 6 || lateMonth == 9 || lateMonth == 11)
-            && lateDay < 30) {
-            lateDay++;
-        } else if (lateMonth == 2) {
-            if (isLeapYear) {
-                if (lateDay < 29) {
-                    lateDay++;
-                } else {
-                    lateDay = 1;
-                    lateMonth = 3;
-                }
-            } else {
-                if (lateDay < 28) {
-                    lateDay++;
-                } else {
-                    lateDay = 1;
-                    lateMonth = 3;
-                }
-            }
-        } else {
-            if (lateMonth < 12) {
-                lateDay = 1;
-                lateMonth++;
-            } else {
-                lateMonth = 1;
-                lateDay = 1;
-                lateYear++;
-            }
-        }
-
-        String lateMo = (Integer.toString(lateMonth).length() < 2 ? "0" + lateMonth : "" + lateMonth);
-        String lateDy = (Integer.toString(lateDay).length() < 2 ? "0" + lateDay : "" + lateDay);
-
-        values.add("" + lateYear + "-" + lateMo + "-" + lateDy);
-        values.add("" + lateMo + "-" + lateDy + "-" + lateYear);
+        values.add(prevDay);
+        values.add(nextDay);
+        values.add(nextDay.substring(5, 7) + "-" + nextDay.substring(8, 10) + "-" + nextDay.substring(0, 4));
+        values.add(getRandomHoliday(toDate(earliest), toDate(latest)));
 
         if (!isNullable) {
             values.add("");
         }
-
         return values;
+    }
+
+    /**
+     *
+     * @param dateString
+     * @return Date denoted by dateString
+     */
+    public static Date toDate(String dateString) {
+        Date date = null;
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            date = df.parse(dateString);
+        } catch (ParseException ex) {
+            System.out.println(ex.fillInStackTrace());
+        }
+        return date;
+    }
+
+    /**
+     *
+     * @return
+     */
+    public String getRandomHoliday(Date earliest, Date latest) {
+        for (String s : EquivalenceClassTransformer.HOLIDAYS) {
+            String dateString = convertToReadableDate(s);
+            if (toDate(dateString).after(earliest) && toDate(dateString).before(latest)) {
+                String st = "asd";
+            }
+        }
+        return null;
+    }
+
+    /**
+     *
+     * @param equivalenceDate
+     * @return
+     */
+    public String convertToReadableDate(String equivalenceDate) {
+        return null;
     }
 
     /**
@@ -279,8 +167,7 @@ public abstract class BoundaryDate<T extends BoundaryActionDate> implements Cust
      * @param variableValue     a list storing the values
      * @return a list of Maps containing the cross product of all states
      */
-    public List<Map<String, String>> returnStates(T action,
-                                                  List<Map<String, String>> possibleStateList, List<String> variableValue) {
+    public List<Map<String, String>> returnStates(T action, List<Map<String, String>> possibleStateList, List<String> variableValue) {
         List<Map<String, String>> states = new LinkedList<>();
         for (Map<String, String> p : possibleStateList) {
             for (String s : variableValue) {
