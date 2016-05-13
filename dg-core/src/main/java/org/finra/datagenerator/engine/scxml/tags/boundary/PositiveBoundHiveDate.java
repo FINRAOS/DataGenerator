10c1b1e9b865eb366ce637597fd4c89f20c927c6
leeny324 <nathaniel.lee289@gmail.com>
2016-05-12 23:54:00 -0400

    positive bound date

--- dg-core/src/main/java/org/finra/datagenerator/engine/scxml/tags/boundary/PositiveBoundHiveDate.java
+++ dg-core/src/main/java/org/finra/datagenerator/engine/scxml/tags/boundary/PositiveBoundHiveDate.java
@@ -51,5 +51,12 @@ public class PositiveBoundHiveDate extends BoundaryDate<PositiveBoundHiveDate.Po
     /**
      * A custom Action for the 'dg:positiveBoundHiveDate' tag inside models
      */
-    public static class PositiveBoundHiveDateTag extends BoundaryActionDate { }
+    public static class PositiveBoundHiveDateTag extends BoundaryActionDate {
+        /**
+         * constructor
+         */
+        public PositiveBoundHiveDateTag() {
+            this.setOnlyBusinessDays("true");
+        }
+    }
 }
